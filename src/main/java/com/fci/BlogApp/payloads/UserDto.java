1a407f2909702fa018c1244fb1ce612979dad43c
Rajan9865 <rajan78800@gmail.com>
2023-04-16 16:38:54 +0530

    hfgfh

--- src/main/java/com/fci/BlogApp/payloads/UserDto.java
+++ src/main/java/com/fci/BlogApp/payloads/UserDto.java
@@ -34,6 +34,6 @@ public class UserDto {
 	private String password;
 	
 	@NotNull
-	@NotEmpty(message = "about is and About can't be null value s")
+	@NotEmpty(message = "about is and About can't be null value jhs")
 	private String about;
 }
