c753db30d61a495c8a37d8af0b2bd783ea436c8a
Rajan9865 <rajan78800@gmail.com>
2023-07-30 00:57:56 +0530

    security ( From Base configration )

--- src/main/java/com/fci/BlogApp/config/SecurityConfig.java
+++ src/main/java/com/fci/BlogApp/config/SecurityConfig.java
@@ -3,15 +3,22 @@
  */
 package com.fci.BlogApp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.fci.BlogApp.Security.CustomUserDetailService;
 
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * @author deby7
+ * @author Rajan.kumar
  *5:46:25 am
  *2023
  *BlogApp
@@ -20,16 +27,37 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .csrf().disable()
-            .authorizeRequests()
-            .anyRequest().authenticated()
-            .and()
-            .httpBasic();
-    }
-}
-
+public class SecurityConfig {
+	
+		@Autowired
+		private CustomUserDetailService customUserDetailService;
+		
+	    @Bean
+	    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	        http
+	        .csrf().disable()
+	        .authorizeHttpRequests()
+	        .anyRequest().authenticated()
+	        .and()
+	        .httpBasic();
+			return http.build();
+	    }
+	    
+//	    @Bean
+	    protected void configure(AuthenticationManagerBuilder auth)
+	    {
+	    	try {
+				auth.userDetailsService(this.customUserDetailService).passwordEncoder(passwordEncoder());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.getMessage();
+				e.printStackTrace();
+			}
+	    }
+	    @Bean
+	    public PasswordEncoder passwordEncoder()
+	    {
+			return new BCryptPasswordEncoder();
+	    }
+	    
+	}
--- src/main/java/com/fci/BlogApp/repositories/UserRepo.java
+++ src/main/java/com/fci/BlogApp/repositories/UserRepo.java
@@ -4,11 +4,14 @@ package com.fci.BlogApp.repositories;
  *6:51:26 am
  */
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fci.BlogApp.entities.User;
 
 public interface UserRepo extends JpaRepository<User, Integer>
 {
+	Optional<User>findByEmail(String email);
 
 }
